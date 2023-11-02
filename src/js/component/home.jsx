dddc57e04f8bc311431b61cc395807c35c0a605d
Luis Salazar O <luiyi_salazar@hotmail.com>
2023-11-02 22:15:53 +0000

    logic added to home.jsx file

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,14 +1,35 @@
-import React from "react";
-import Lights from "./lights.jsx";
+import React, { useState } from "react";
+import Lights from "./lights";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const TrafficLight = () => {
+  const [clickedColor, setClickedColor] = useState("");
+
+  const handlerRedClick = () => {
+    setClickedColor("red");
+    console.log("You clicked the Red color");
+  };
+
+  const handlerYellowClick = () => {
+    setClickedColor("yellow");
+    console.log("You clicked the Yellow color");
+  };
+
+  const handlerGreenClick = () => {
+    setClickedColor("green");
+    console.log("You clicked the Green color");
+  };
+
   return (
     <React.Fragment>
-      <Lights />
+      <Lights
+        redLightClick={handlerRedClick}
+        yellowLightClick={handlerYellowClick}
+        greenLightClick={handlerGreenClick}
+      />
     </React.Fragment>
   );
 };
